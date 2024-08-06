8e71220549e74506b1890d9e8a2776c8418824c5
thiago082882 <thiagocostadejesus2@gmail.com>
2024-08-06 17:10:36 -0300

    aplicativo 03

---
+++ 03_flashlight/src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {CapacitorFlash} from '@capgo/capacitor-flash'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public active : boolean;
+
+  constructor() {
+ this.active = false;
+}
+
+flash(){
+this.active = !this.active;
+
+if(this.active){
+  CapacitorFlash.switchOn({intensity:100});
+}else{
+  CapacitorFlash.switchOff();
+}
+}
+}
